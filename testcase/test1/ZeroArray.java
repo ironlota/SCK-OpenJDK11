89f4313d35f860cf4e357ef198028028eaa4e858
Ray Andrew <raydreww@gmail.com>
2019-10-15 10:02:33 +0700

    change testcase into byte

--- testcase/test1/ZeroArray.java
+++ testcase/test1/ZeroArray.java
@@ -3,34 +3,35 @@ package testcase.test1;
 import java.util.Arrays;
 
 public class ZeroArray {
-    public static void main(String[] args) {
-        int[] src = new int[1500];
-        int[] dst = new int[src.length];
-        // for (String argss : args) {
-        //     System.out.println(argss);
-        // }
+  public static void main(String[] args) {
+    byte[] src = new byte[4096000];
+    byte[] dst = new byte[src.length];
 
-        // for (int elem: myarray) {
-        // elem = Integer.parseInt(args[0]);
-        // elem = 4;
-        // }
+    // for (String argss : args) {
+    //     System.out.println(argss);
+    // }
 
-        System.out.println("Phase 1");
-      
-        Arrays.fill(src, Integer.parseInt(args[0]));
+    // for (int elem: myarray) {
+    // elem = Integer.parseInt(args[0]);
+    // elem = 4;
+    // }
 
-        System.out.println("Phase 2, Copy array");
+    System.out.println("Phase 1");
 
-        System.arraycopy(src, 0, dst, 0, src.length);
+    Arrays.fill(src, (byte) 1);
 
-        System.out.println("Phase 3");
-        // System.out.println("Hello World!");
+    System.out.println("Phase 2, Copy array");
 
-        // System.gc();
-        
-        while(true) {
-            // noop
-        }
+    System.arraycopy(src, 0, dst, 0, src.length);
+
+    System.out.println("Phase 3");
+
+    System.out.println("src == dst : " + Arrays.equals(src, dst));
+
+    // System.gc();
+
+    while (true) {
+      // noop
     }
+  }
 }
-
