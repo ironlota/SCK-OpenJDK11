6f8fa908e274b72c17550ec1ad59791566d22a3f
Ray Andrew <raydreww@gmail.com>
2019-10-15 14:37:14 +0700

    [WIP] fix calling posix_memalign

--- sources/src/hotspot/share/runtime/os.cpp
+++ sources/src/hotspot/share/runtime/os.cpp
@@ -702,7 +702,9 @@ void* os::malloc(size_t size, MEMFLAGS memflags, const NativeCallStack& stack) {
   // add this line to change the implementation of malloc so it can allocate
   // page-aligned memory
   long page_size = sysconf(_SC_PAGE_SIZE);
-  ::posix_memalign((void**) &ptr, page_size, alloc_size);
+  if(!::posix_memalign((void**) &ptr, page_size, alloc_size)) {
+      return NULL;
+  }
 
 #ifdef ASSERT
   if (ptr == NULL) {
